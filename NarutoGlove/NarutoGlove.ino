8829161abb5876b2a8eb1a142f025129aeabfa98
Brian Nguyen <nguyen.brian70@gmail.com>
2019-02-01 15:55:14 -0500

    Changed keys

--- NarutoGlove/NarutoGlove.ino
+++ NarutoGlove/NarutoGlove.ino
@@ -1,6 +1,6 @@
 /*
    DIGF 2004 Atelier 1
-   Brian Nguyen, Madelaine Fischer-Bernhut, Salisa Jatuweerapong 
+   Brian Nguyen, Madelaine Fischer-Bernhut, Salisa Jatuweerapong
    Experiment 3 - Final Prototype
    Naruto Glove Game Controller
 */
@@ -23,7 +23,7 @@ const int pinCount = 6;
 // Integer array that holds the pin numbers of the glove controller
 int inputPins[pinCount - 2] = {2, 3, 4, 5};
 // Character array that holds the keys to be pressed
-char controlKeys[pinCount] = {'i', 'o', 'p', 'u', 'a', 'd'};
+char controlKeys[pinCount] = {'i', 'p', 'o', 'u', 'a', 'd'};
 // Array of strings, use of * means it is a array of strings
 // Used for debugging
 char* controlNames[pinCount - 2] = {"Dog", "Ox", "Ram", "Boar"};
@@ -48,39 +48,40 @@ void setup() {
     pinMode(inputPins[i], INPUT_PULLUP);
   }
 
-  
- while (millis() < 5000) {
-   int leftValue = analogRead(A0);
-   int rightValue = analogRead(A1);
 
-   // record the maximum sensor value
-   if (leftValue > leftMax) {
-     leftMax = leftValue;
-   }
+  // Calibration phase
+  while (millis() < 5000) {
+    int leftValue = analogRead(A0);
+    int rightValue = analogRead(A1);
 
-   // record the minimum sensor value
-   if (leftValue < leftMin) {
-     leftMin = leftValue;
-   }
+    // record the maximum sensor value
+    if (leftValue > leftMax) {
+      leftMax = leftValue;
+    }
 
-      // record the maximum sensor value
-   if (rightValue > rightMax) {
-     rightMax = rightValue;
-   }
+    // record the minimum sensor value
+    if (leftValue < leftMin) {
+      leftMin = leftValue;
+    }
 
-   // record the minimum sensor value
-   if (rightValue < rightMin) {
-     rightMin = rightValue;
-   }
- }
-  
+    // record the maximum sensor value
+    if (rightValue > rightMax) {
+      rightMax = rightValue;
+    }
+
+    // record the minimum sensor value
+    if (rightValue < rightMin) {
+      rightMin = rightValue;
+    }
+  }
+
 }
 
 // Main loop
 void loop() {
-   int leftValue = analogRead(A0);
-   int rightValue = analogRead(A1);
-  
+  int leftValue = analogRead(A0);
+  int rightValue = analogRead(A1);
+
   // Integer array that contians the state of every glove pin
   // 0 = Off
   // 1 = On
@@ -100,12 +101,12 @@ void loop() {
   for (int i = 0; i < pinCount; i++) {
     // Edgecases that stops a key from being pressed accidentally
     // Happens when some hand signs have two sensors touching at the same time
-//    if (i == 3 && inputState[0] == LOW) {
-//      continue;
-//    }
-//    if (i == 3 && inputState[1] == LOW) {
-//      continue;
-//    }
+    //    if (i == 3 && inputState[0] == LOW) {
+    //      continue;
+    //    }
+    //    if (i == 3 && inputState[1] == LOW) {
+    //      continue;
+    //    }
     if (i == 2 && inputState[1] == LOW) {
       continue;
     }
@@ -127,46 +128,46 @@ void loop() {
       keyPressed = true;
     }
   }
-  
+
   leftValue = map(leftValue, leftMin, leftMax, 0, 255);
   rightValue = map(rightValue, rightMin, rightMax, 0, 255);
-  
+
   leftValue = constrain(leftValue, 0, 255);
   rightValue = constrain(rightValue, 0, 255);
 
 
   // If statements that check if a person has pressed on the movement pad
   // Left control
-  if(leftValue > 200) {
+  if (leftValue > 200) {
     Serial.println("Pressing : A");
     Keyboard.press('a');
     keyPressed = true;
-    
+
   }
   Serial.println(leftValue);
-    Serial.println(rightValue);
+  Serial.println(rightValue);
   Serial.println(analogRead(A0));
-    Serial.println(analogRead(A1));
+  Serial.println(analogRead(A1));
 
   // Right control
-  if(rightValue > 200) {
+  if (rightValue > 200) {
     Serial.println("Pressing : D");
     Keyboard.press('d');
     keyPressed = true;
   }
 
-  // Delay of 10ms 
+  // Delay of 10ms
   // Required otherwise the game wont recognize a key has been pressed
   delay(10);
 
   // If keyPressed is true, loop through all the keys and releases them
   if (keyPressed == true) {
     for (int i = 0; i < pinCount; i++) {
-     // Debug lines
-//     Serial.print("Releasing: ");
-//     Serial.println(controlKeys[i]);
-     // Releases the key
-     Keyboard.release(controlKeys[i]);
+      // Debug lines
+      //     Serial.print("Releasing: ");
+      //     Serial.println(controlKeys[i]);
+      // Releases the key
+      Keyboard.release(controlKeys[i]);
     }
   }
 }
